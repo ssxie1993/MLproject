004cbcd390400af7c04ac3a2000962c8eacf2fbb
TulasiGogineni <44048530+TulasiGogineni@users.noreply.github.com>
2018-12-03 12:04:29 -0800

    Update predict_snp500.py
    Included ARIMA function module with a small problem with date management

--- predict_snp500.py
+++ predict_snp500.py
@@ -11,7 +11,7 @@ import os
 import numpy as np
 import tensorflow as tf # This code has been tested with TensorFlow 1.6
 import statsmodels.api as sm
-from fbprophet import Prophet
+#from fbprophet import Prophet
 from scipy import stats
 from pandas.core import datetools
 from keras.models import Sequential
@@ -69,6 +69,29 @@ def processData(data,lb):
         Y.append(data[(i+lb),0])
     return np.array(X),np.array(Y)
 
+from statsmodels.tsa.stattools import adfuller
+def test_stationarity(timeseries):
+    
+    #Determing rolling statistics
+    rolmean = timeseries.rolling(window=12).mean() #(timeseries, window=12)
+    rolstd = timeseries.rolling(window=12).std()
+    
+    #Plot rolling statistics:
+    plt.plot(timeseries, color='blue',label='Original')
+    plt.plot(rolmean, color='red', label='Rolling Mean')
+    plt.plot(rolstd, color='black', label = 'Rolling Std')
+    plt.legend(loc='best')
+    plt.title('Rolling Mean & Standard Deviation')
+    plt.show(block=False)
+    
+    #Perform Dickey-Fuller test:
+    print ('Results of Dickey-Fuller Test:')
+    dftest = adfuller(timeseries, autolag='AIC')
+    dfoutput = pd.Series(dftest[0:4], index=['Test Statistic','p-value','#Lags Used','Number of Observations Used'])
+    for key,value in dftest[4].items():
+        dfoutput['Critical Value (%s)'%key] = value
+    print (dfoutput)
+    
 def runLSTM(ticker, data, epochs):
 
 #   plotTicker(ticker, data)
@@ -245,7 +268,7 @@ def runprophet(df, ticker):
    
    plt.show()
    
-   fig2 = m.plot_components(forecast)
+   m.plot_components(forecast)
    plt.show()
    
    # Monthly Data Predictions
@@ -372,12 +395,166 @@ def runprophet(df, ticker):
    
    plt.show()
    
-   fig2 = m.plot_components(forecast)
+   m.plot_components(forecast)
    plt.show()
-   
-#####END PROPHET
+#   
+######END PROPHET
 
+###############################################################
+#Merging the code here  -- Tulasi 
+
+def runARIMA(df, ticker):
+    dateparse = lambda dates: pd.datetime.strptime(dates, '%Y-%m-%d')
+    data_3 = pd.read_csv('C:/Users/Tulasi/Documents/Tulasi/MLPROJECT/all_stocks_5yr.csv', parse_dates=['date'], index_col='date',date_parser=dateparse)
+    print(data_3.head())
+    data_3.head()
+    data_3.dtypes
+    print(data_3.index)
+
+    ts = data_3[data_3['Name']== ticker].close
+
+    #Testing the stationarity of the original time series
+    test_stationarity(ts)
+
+#Estimating & Eliminating Trend
+#plt.plot(ts)
+    ts_log = np.log(ts)
+#plt.plot(ts_log)
+
+# Moving average for 5
+    moving_avg = ts_log.rolling(window=12).mean()
+    plt.plot(ts_log, color='blue')
+    plt.plot(moving_avg, color='red')
+    plt.show()
+    
+    ts_log_moving_avg_diff = ts_log - moving_avg
+    ts_log_moving_avg_diff.head(12)
+    
+    #Testing the stationarity of the error = logarithmic data -moving avg of logarithmic data
+    ts_log_moving_avg_diff.dropna(inplace=True)
+    test_stationarity(ts_log_moving_avg_diff)
+
+
+##Testing the stationarity after exponentially weighted ma
+
+
+    expwighted_avg = ts_log.ewm(halflife=12).mean()
+    plt.plot(ts_log)
+    plt.plot(expwighted_avg, color='red')
+    plt.show()
+    
+    ts_log_ewma_diff = ts_log - expwighted_avg
+    test_stationarity(ts_log_ewma_diff)
+    
+
+
+    #Eliminating Trend and Seasonality
+    #The simple trend reduction techniques discussed before don’t work in all cases, particularly the ones with high seasonality. Lets discuss two ways of removing trend and seasonality:
+    #
+    #Differencing – taking the differece with a particular time lag
+    #Decomposition – modeling both trend and seasonality and removing them from the model.
+    
+    ts_log_diff = ts_log - ts_log.shift()
+    plt.plot(ts_log_diff)
+    plt.show()
+    
+    ts_log_diff.dropna(inplace=True)
+    test_stationarity(ts_log_diff)
+    
+    #Decomposition
+    from statsmodels.tsa.seasonal import seasonal_decompose
+    decomposition = seasonal_decompose(ts_log,model='additive',freq=1)
+    
+    trend = decomposition.trend
+    seasonal = decomposition.seasonal
+    residual = decomposition.resid
+    
+    plt.subplot(411)
+    plt.plot(ts_log, label='Original')
+    plt.legend(loc='best')
+    plt.subplot(412)
+    plt.plot(trend, label='Trend')
+    plt.legend(loc='best')
+    plt.subplot(413)
+    plt.plot(seasonal, label='Seasonal')
+    plt.legend(loc='best')
+    plt.subplot(414)
+    plt.plot(residual, label='Residuals')
+    plt.legend(loc='best')
+    plt.tight_layout()
+    plt.show()
+    #check stationarity of residuals:
+    #The Dickey-Fuller test statistic is significantly lower than the 1% critical value. So this TS is very close to stationary. 
+    ts_log_decompose = residual
+    ts_log_decompose.dropna(inplace=True)
+    test_stationarity(ts_log_decompose)
+    
+    #ACF and PACF plots:
+    from statsmodels.tsa.stattools import acf, pacf
+    lag_acf = acf(ts_log_diff, nlags=20)
+    lag_pacf = pacf(ts_log_diff, nlags=20, method='ols')
+    #Plot ACF: 
+    plt.subplot(121) 
+    plt.plot(lag_acf)
+    plt.axhline(y=0,linestyle='--',color='gray')
+    plt.axhline(y=-1.96/np.sqrt(len(ts_log_diff)),linestyle='--',color='gray')
+    plt.axhline(y=1.96/np.sqrt(len(ts_log_diff)),linestyle='--',color='gray')
+    plt.title('Autocorrelation Function')
+    
+    #Plot PACF:
+    plt.subplot(122)
+    plt.plot(lag_pacf)
+    plt.axhline(y=0,linestyle='--',color='gray')
+    plt.axhline(y=-1.96/np.sqrt(len(ts_log_diff)),linestyle='--',color='gray')
+    plt.axhline(y=1.96/np.sqrt(len(ts_log_diff)),linestyle='--',color='gray')
+    plt.title('Partial Autocorrelation Function')
+    plt.tight_layout()
+    plt.show()
+    #Perform Augmented Dickey–Fuller test:
+    #from statsmodels.tsa.stattools import adfuller
+    from statsmodels.tsa.stattools import acf, pacf
+    from statsmodels.tsa.seasonal import seasonal_decompose
+    from statsmodels.tsa.arima_model import ARIMA
+    
+    #Combined Model
+    model = ARIMA(ts_log, order=(0,1,1))
+    
+    results_ARIMA = model.fit(disp=-1)  
+    plt.plot(ts_log_diff)
+    plt.plot(results_ARIMA.fittedvalues, color='green')
+    plt.title('RSS: %.4f'% sum((results_ARIMA.fittedvalues-ts_log_diff)**2))
+    plt.show()
+    
+    
+    predictions_ARIMA_diff = pd.Series(results_ARIMA.fittedvalues, copy=True)
+    predictions_ARIMA_diff.head()
+    
+    predictions_ARIMA_diff_cumsum = predictions_ARIMA_diff.cumsum()
+    predictions_ARIMA_diff_cumsum.head()
+    
+    predictions_ARIMA_log = pd.Series(ts_log.iloc[0], index=ts_log.index)
+    predictions_ARIMA_log = predictions_ARIMA_log.add(predictions_ARIMA_diff_cumsum,fill_value=0)
+    predictions_ARIMA_log.head()
+    
+    predictions_ARIMA = np.exp(predictions_ARIMA_log)
+    plt.plot(ts_log)
+    plt.plot(predictions_ARIMA, color='black')
+    plt.title('RMSE: %.4f'% np.sqrt(sum((predictions_ARIMA-ts)**2)/len(ts)))
+    plt.show()
+    
+    results_ARIMA.plot_predict(1,264) 
+    results_ARIMA.forecast(steps=120)
+    #plt.plot(moving_avg, color='black')
+    plt.show()
+    
+    #results_ARIMA.plot_predict() 
+    #x=results_ARIMA.forecast()
+    #plt.plot(moving_avg, label='movavg', color='magenta')
+    #plt.show()
+
+################################################################
 stockdata = preprocessdata()
 #print ("DEBUG: stockdata size:", stockdata.shape[0])
 runLSTM("AMZN", stockdata, 30)
 runprophet(stockdata, "AMZN")
+runARIMA(stockdata,"AMZN")
